8c8293048b636dec7663f33175fc58b096c8c1e0
Anton Bondar <direx187@gmail.com>
2023-03-09 12:02:36 +0200

    divs shows

--- pages/index.js
+++ pages/index.js
@@ -21,18 +21,24 @@ export default function Home() {
         <link rel="stylesheet" href="/styles.css"></link>
       </Head>
       <div>
-        <form onSubmit={handleSearch} className ="flex items-center justify-center">
+        <form
+          onSubmit={handleSearch}
+          className="flex items-center justify-center"
+        >
           <input
             type="text"
             value={searchTerm}
             onChange={(event) => setSearchTerm(event.target.value)}
-            className = "px-4 py-2 border border-gray-400 rounded-lg w-64"
+            className="px-4 py-2 border border-gray-400 rounded-lg w-64"
           />
-          <button type="submit" className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-700 focus:bg-blue-700 focus:outline-none transition duration-300 ease-in-out">
+          <button
+            type="submit"
+            className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-700 focus:bg-blue-700 focus:outline-none transition duration-300 ease-in-out"
+          >
             Search
           </button>
         </form>
-        <div className="movie_list">
+        <div className="flex flex-nowrap overflow-x-auto whitespace-no-wrap max-w-screen-lg mx-auto">
           {movies.map((movie) => (
             <div key={movie.imdbID} className="movie_item">
               <h1>{movie.Title}</h1>
