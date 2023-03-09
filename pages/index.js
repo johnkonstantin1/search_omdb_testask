de12509a49203d0d45936cfc5b3df81e01566c82
Anton Bondar <direx187@gmail.com>
2023-03-09 11:58:24 +0200

    tailwind simple

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import "../public/styles.module.css";
+import Head from "next/head";
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -16,26 +16,32 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value)}
-        />
-        <button type="submit" className="button-submit">
-          Search
-        </button>
-      </form>
-      <div className="movie--list">
-        {movies.map((movie) => (
-          <div key={movie.imdbID} className="movie--item">
-            <h1>{movie.Title}</h1>
-            <p>Year:{movie.Year}</p>
-            <img className="movie-poster" src={movie.Poster}></img>
-          </div>
-        ))}
+    <>
+      <Head>
+        <link rel="stylesheet" href="/styles.css"></link>
+      </Head>
+      <div>
+        <form onSubmit={handleSearch} className ="flex items-center justify-center">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+            className = "px-4 py-2 border border-gray-400 rounded-lg w-64"
+          />
+          <button type="submit" className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-700 focus:bg-blue-700 focus:outline-none transition duration-300 ease-in-out">
+            Search
+          </button>
+        </form>
+        <div className="movie_list">
+          {movies.map((movie) => (
+            <div key={movie.imdbID} className="movie_item">
+              <h1>{movie.Title}</h1>
+              <p>Year:{movie.Year}</p>
+              <img className="movie_poster" src={movie.Poster}></img>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
